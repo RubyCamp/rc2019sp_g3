b44312cb55d8c424023a645aaffec49e608f8fdd
Koki Shimoyama <37096443+koutakakaho@users.noreply.github.com>
2019-03-07 13:27:03 +0900

    ポイの表示

--- our_game/scenes/game/director.rb
+++ our_game/scenes/game/director.rb
@@ -1,14 +1,28 @@
-	# #同じディレクトリ内にあるファイルを読み込めるようにする
+#同じディレクトリ内にあるファイルを読み込めるようにする
 require_relative "player"
+require "time"
 module Game
 	class Director
 
 		def initialize()
-			@space = CP::Space.new
-	    	@space.gravity = CP::Vec2.new(0, 0)
+			#ひとつ目の空間
+			@space_fish = CP::Space.new
+	    	@space_fish.gravity = CP::Vec2.new(0, 0)
+
+	    	#二つ目の空間
+	    	@space_poi = CP::Space.new
+	    	@space_poi.gravity = CP::Vec2.new(0,0)
+
 			@fishes = []
 			@font = Font.new(32)
 			@score = 0
+			@limit_time = 100
+			@loop_count = 0
+			@time = Time.now
+
+			image = Image.load("images/poi1.png")
+			@player= Player.new(30,300,50,10, image)
+			@space_poi.add(@player)
 			#　1から10までの数の金魚を発生させる
 			# (rand(9)+1).times do
 			10.times do
@@ -17,14 +31,14 @@ module Game
 				  rand(400)+100,
 				  40
 				)
-				@space.add(fish)
+				@space_fish.add(fish)
 				@fishes << fish
 			end
 			#　ステージの壁の作成
-			CPBase.generate_walls(@space, Window.width,Window.height)
+			CPBase.generate_walls(@space_fish, Window.width,Window.height)
 
 			#　衝突処理
-			@space.add_collision_handler(Fish::COLLISION_TYPE, Fish::COLLISION_TYPE) do |a, b, arb|
+			@space_fish.add_collision_handler(Fish::COLLISION_TYPE, Fish::COLLISION_TYPE) do |a, b, arb|
 		     # 衝突個所（arb.points配列）から、先頭の1つを取得（複数個所ぶつかるケースもあり得るため配列になっている）
 		    	pos = arb.points.first.point
 		     # 衝突個所の座標に絵を表示（1フレームで消える点に留意）
@@ -34,13 +48,24 @@ module Game
 	    end
 
 		def play
+			
+			@player.move
+			@player.draw
 			@fishes.each do |fish|
 			#　金魚を動かす
 				fish.move
 				fish.draw
 			end
-			Window.draw_font(0,0, "スコア：#{@score}", @font)
-			@space.step( 1/ 60.0 )
+			Window.draw_font(0,0, "スコア：#{@score}, 時間:#{@limit_time}, #{@time}", @font)
+			@space_fish.step( 1/ 60.0 )
+			@space_poi.step (1 / 60.0 )
+			@loop_count += 1
+			if @loop_count % 30 == 0
+				@limit_time -= 1
+				if @limit_time == 0
+					
+				end
+			end
 			#scene_transition
 		end
 
